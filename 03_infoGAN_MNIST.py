485b6aef4a8681c2613b928b09dbd863176aa39a
hyogun <gunsbrother@naver.com>
2018-06-20 20:23:24 +0900

    infogan 완성 1

--- 03_infoGAN_MNIST.py
+++ 03_infoGAN_MNIST.py
@@ -13,6 +13,7 @@ learning_rate = 2e-4
 training_epoch = 20
 beta1 = .5
 
+
 def discriminator(x, isTrain=True, reuse=False, with_Q=False):
 
     with tf.variable_scope(name_or_scope='Dis', reuse=reuse):
@@ -27,10 +28,11 @@ def discriminator(x, isTrain=True, reuse=False, with_Q=False):
             return D
 
         else:
-            dense2 = tf.layers.dense(dense1, 128, None, False)
-            dense2 = tf.layers.batch_normalization(dense2, training=isTrain)
-            dense2 = tf.nn.leaky_relu(dense2)
-            Q = tf.layers.dense(dense2, 12)  # logit
+            with tf.variable_scope(name_or_scope='Con', reuse=reuse):
+                dense2 = tf.layers.dense(dense1, 128, None, False)
+                dense2 = tf.layers.batch_normalization(dense2, training=isTrain)
+                dense2 = tf.nn.leaky_relu(dense2)
+                Q = tf.layers.dense(dense2, 12)  # logit
             return D, Q
 
 
@@ -45,22 +47,31 @@ def generator(z, c, isTrain=True, reuse=False):
         dense2 = tf.reshape(dense2, [None, 7, 7, 128])
         conv1 = tf.layers.conv2d_transpose(dense2, 64, 4, (2, 2), 'same', activation=tf.nn.relu, use_bias=False)
         conv1 = tf.layers.batch_normalization(conv1, training=isTrain)
-        conv2 = tf.layers.conv2d_transpose(conv1, 1, 4, (2, 2), 'same')  # 논문엔 따로 얘기 없지 stride 2여야 됨
+        conv2 = tf.layers.conv2d_transpose(conv1, 1, 4, (2, 2), 'same')  # 논문엔 따로 얘기 없지만 stride 2여야 됨
 
-        return conv2
+        return tf.nn.sigmoid(conv2)
 
 
 def random_noise(batch_size):
-    return np.random.normal(0, 1, size=[batch_size, 62])
+    return np.random.uniform(-1, 1, size=[batch_size, 62])
 
 
-def random_condition(batch_size):
-    categorical = np.random.multinomial(1, [1/10] * 10, batch_size)
-    continuous = np.random.uniform(-1, 1, size=[batch_size, 2])
+def random_code(batch_size):
+    categorical = random_categorical(batch_size)
+    continuous = random_continuous(batch_size)
     return np.concatenate([categorical, continuous], axis=1)
 
 
+def random_categorical(batch_size):
+    return np.random.multinomial(1, [1/10] * 10, batch_size)
+
+
+def random_continuous(batch_size):
+    return np.random.uniform(-1, 1, size=[batch_size, 2])
+
+
 global_time0 = time.time()
+
 g = tf.Graph()
 with g.as_default():
 
@@ -73,6 +84,19 @@ with g.as_default():
     fake_logits, Q_logits = discriminator(fake_x, isTrain=isTrain, with_Q=True)
     real_logits = discriminator(X, isTrain=isTrain, reuse=True)
 
+    d_loss_real = tf.reduce_mean(
+        tf.nn.sigmoid_cross_entropy_with_logits(
+            logits=real_logits,
+            labels=tf.ones_like(real_logits)
+        )
+    )
+    d_loss_fake = tf.reduce_mean(
+        tf.nn.sigmoid_cross_entropy_with_logits(
+            logits=fake_logits,
+            labels=tf.zeros_like(fake_logits)
+        )
+    )
+    d_loss = d_loss_real + d_loss_fake
 
     g_loss_base = tf.reduce_mean(
         tf.nn.sigmoid_cross_entropy_with_logits(
@@ -82,7 +106,106 @@ with g.as_default():
     )
     q_loss_disc = tf.reduce_mean(
         tf.nn.sigmoid_cross_entropy_with_logits(
-            logits=Q_logits,
-            labels=tf.zeros([batch_size, 10])
+            logits=Q_logits[:, :10],
+            labels=C[:, :10]
         )
-    ) +
+    )  # categorical은 확률 space이고 continuous는 아니기 때문에 continuous는 logit을 그대로 씀
+    q_loss_cont = tf.reduce_mean(tf.reduce_sum(
+            tf.square(Q_logits[:, 10:] - C[:, 10:])
+        ), axis=1
+    )
+    q_loss = q_loss_disc + q_loss_cont
+    g_loss = g_loss_base + q_loss
+
+    t_vars = tf.trainable_variables()
+    g_vars = [var for var in t_vars if 'Gen' in var.name]
+    d_vars = [var for var in t_vars if 'Dis' in var.name]
+    q_vars = [var for var in t_vars if any(x in var.name for x in ['Gen', 'Dis', 'Con'])]
+
+    optimizer = tf.train.AdamOptimizer(learning_rate, beta1=beta1)
+
+    d_opt = optimizer.minimize(d_loss, var_list=d_vars)
+    g_opt = optimizer.minimize(g_loss, var_list=g_vars)
+    q_opt = optimizer.minimize(q_loss, var_list=q_vars)
+
+    saver = tf.train.Saver(max_to_keep=10)
+
+    summary_g = tf.summary.merge(
+        [tf.summary.scalar(tf.summary.scalar('g_loss', g_loss), 'd_loss_fake', d_loss_fake)])
+    summary_d = tf.summary.merge(
+        [tf.summary.scalar(tf.summary.scalar('d_loss', d_loss), 'd_loss_real', d_loss_real)])
+    summary_q = tf.summary.merge(
+        [tf.summary.scalar('q_loss', q_loss),
+         tf.summary.scalar('q_loss_disc', q_loss_disc),
+         tf.summary.scalar('q_loss_cont', q_loss_cont)])
+    writer = tf.summary.FileWriter('infoGAN/Summaries')
+
+with tf.Session(graph=g) as sess:
+
+    sess.run(tf.global_variables_initializer())
+    total_batches = len(train_x) // batch_size
+
+    sample_noise = random_noise(50)
+    # 0, 1, 2, 3, 4 one-hot vector 각각 열 개씩
+    sample_categorical = np.eye(10)[(np.ones(50, np.int32).reshape(-1, 5) * np.arange(5)).T.flatten()]
+    sample_continuous = random_continuous(50)
+    sample_code = np.concatenate([sample_categorical, sample_continuous], axis=1)
+
+    global_step = 0
+
+    for epoch in range(training_epoch):
+
+        epoch_time0 = time.time()
+
+        for batch in range(total_batches):
+
+            t0 = time.time()
+
+            noise = random_noise(batch_size)
+            code = random_code(batch_size)
+            batch_x = train_x[batch * batch_size:(batch + 1) * batch_size]
+            sess.run(d_opt, feed_dict={X: batch_x, Z: noise, C: code, isTrain: True})
+            sess.run(g_opt, feed_dict={Z: noise, C: code, isTrain: True})
+            sess.run(q_opt, feed_dict={X: batch_x, Z: noise, C: code, isTrain: True})
+
+            t1 = time.time()
+
+            gl, dl, ql = sess.run(
+                [g_loss, d_loss, q_loss],
+                feed_dict={X: batch_x, Z: noise, C: code, isTrain: False})
+
+            gs, ds, qs = sess.run([summary_g, summary_d, summary_q],
+                               feed_dict={X: batch_x, Z: noise, C: code, isTrain: False})
+            writer.add_summary(gs, global_step)
+            writer.add_summary(ds, global_step)
+            writer.add_summary(qs, global_step)
+
+            print('epoch : {:02d}/{:02d}'.format(epoch + 1, training_epoch), end=' | ')
+            print('batch : {:03d}/{:03d}'.format(batch + 1, total_batches), end=' | ')
+            print('time spent : {:.3f} sec'.format(t1 - t0), end=' | ')
+            print('gl : {:.6f} | dl : {:.6f} | ql : {:.6f}'.format(gl, dl, ql))
+
+            global_step += 1
+
+        epoch_time1 = time.time()
+        epoch_time = epoch_time1 - epoch_time0
+        print('==================== EPOCH :', epoch + 1, ' has ended ====================')
+        print('Generator Loss : {:.6f}\nDiscriminator Loss : {:.6f}'.format(gl, dl))
+        print('Auxiliary Distribution Loss : {:.6f}'.format(ql))
+        print('Time Spent in This Epoch : {:.3f}'.format(epoch_time))
+
+        generated = sess.run(fake_x, feed_dict={Z: sample_noise, C: sample_code, isTrain: False})
+        fig, ax = plt.subplots(5, 10, figsize=(5, 10))
+        for i in range(5):
+            for j in range(10):
+                ax[i].set_axis_off()
+                ax[i].imshow(generated[i*10+j, :, :, 0])
+        plt.savefig('infoGAN/Images/{:3d}.png'.format(epoch+1), bbox_inches='tight')
+        plt.close(fig)
+
+        saver.save(sess, 'infoGAN/Checkpoints/model', global_step=epoch)
+
+    global_time1 = time.time()
+    global_time_spent = global_time1 - global_time0
+    print(('=' * 40 + '\n') * 3, 'Optimization Has Been Completed!!')
+    print('Total Time Spent : {:.3f}'.format(global_time_spent))
